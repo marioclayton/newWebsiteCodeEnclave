fe561af2e2a8062af093afdfe8abfbe7609baff7
Mario Clayton <mclaytongamero96@gmail.com>
2024-11-16 23:28:40 -0500

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import Logo from '../assets/logo.png';
 
 const Navbar = () => {
 
@@ -11,7 +12,8 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      {/*<h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>*/}
+      <img className='w-[120px]  my-4' src={Logo} alt='/' />
       <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
         <li className='p-4'>Company</li>
@@ -23,7 +25,7 @@ const Navbar = () => {
         {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
       </div>
       <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <img className='w-[120px]  my-4' src={Logo} alt='/' />  
           <ul className='uppercase p-4'>
             <li className='p-4 border-b border-gray-600'>Home</li>
             <li className='p-4 border-b border-gray-600'>Company</li>
