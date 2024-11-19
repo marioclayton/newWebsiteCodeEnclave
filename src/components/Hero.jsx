c879726901d0b124ca8f693a210bf0a362089d5b
Mario Clayton <mclaytongamero96@gmail.com>
2024-11-19 10:16:56 -0500

    added motion to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {motion} from 'framer-motion';
 
 const Hero = () => {
   return (
@@ -8,7 +8,7 @@ const Hero = () => {
 
 
     <div className='text-white overlay-container '>
-      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center overlay-content'>
+      <motion.div initial={{x:-0, opacity:0}} whileInView={{ x: 0, opacity:1 }} transition={{duration:1, delay:0.2}}  className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center overlay-content'>
         <p className='bg-gradient-to-r from-green-300 via-slate-500 to-yellow-500 bg-clip-text text-xl tracking-tight text-transparent  p-2'>
           GROWING YOUR BUSINESS IN THE ONLINE WORLD
         </p>
@@ -22,7 +22,7 @@ const Hero = () => {
         </div>
         <p className='md:text-2xl text-xl  bg-gradient-to-r from-green-300 via-slate-500 to-yellow-500 bg-clip-text tracking-tight text-transparent py-10'>We specialize in helping small and medium-sized businesses establish a strong and effective online presence.</p>
         <button className='animate-pulse bg-[#ffffff] w-[200px] rounded-md font-medium my-6 mx-auto text-xl py-3 text-black'>Get Started</button>
-      </div>
+      </motion.div>
     </div>
   );
 };
