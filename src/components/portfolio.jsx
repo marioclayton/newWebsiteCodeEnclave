4aed85904f023caa2848cc3743eb0246d8abf0fa
Mario Clayton <mclaytongamero96@gmail.com>
2025-05-26 23:45:52 -0400

    added projects

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -56,7 +56,9 @@ const portfolio = () => {
                             </p>
                             <a href={link.href} title=""
                             className="text-white  justify-center inline-flex items-center  focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-primary-800"
-                            role="button">
+                            role="button"
+                            target="_blank" rel="noopener noreferrer"
+                            >
                             View case study
                             <svg aria-hidden="true" className="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                 fill="currentColor">
--- src/constants/portfolio.js
+++ src/constants/portfolio.js
@@ -1,12 +1,28 @@
 import Portfolio1 from '../assets/project1.png';
+import Portfolio2 from '../assets/project2.png';
+import Portfolio3 from '../assets/project3.png';
 
 export const Projects = [
     { 
         company: "VS Shining Stars Cleaning", 
         name: "Cleaning website", 
-        description: "Used Wordpress to build the website, and optimized it for serch engines", 
+        description: "Used Wordpress to build the website", 
         image: Portfolio1, 
         href: "https://vsshiningstars.com",
+    },
+    { 
+        company: "Resplandece Mujer", 
+        name: "Online Blog", 
+        description: "Used Next.js and contentful to build the website, and optimized it for SEO", 
+        image: Portfolio2, 
+        href: "https://resplandecemujer.com",
+    },
+    { 
+        company: "Blue Sky Elite Investments", 
+        name: "Real Estate website", 
+        description: "Used Next.js to build the website, and optimized it for SEO", 
+        image: Portfolio3, 
+        href: "https://blueskyelite.com/",
     },
     
     
